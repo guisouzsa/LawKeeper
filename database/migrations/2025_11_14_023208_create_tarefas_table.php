d2fdd8d2b6b7ef6a5d9c74b8a21e348dcefe9d63
guisouzsa <guilherme.souza103@aluno.ce.gov.br>
2025-11-13 23:41:49 -0300

    Migrations: criar tabela tarefas
    Estrutura da tabela tarefas com campos: titulo, data_limite, status (enum) e descricao.

---
+++ database/migrations/2025_11_14_023208_create_tarefas_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo');             
+            $table->date('data_limite');         
+            $table->enum('status', ['Pendente', 'Concluída', 'Atrasada']); 
+            $table->text('descricao')->nullable(); // Descrição opcional
+            $table->timestamps(); 
+        });
+    }
+
+    
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
