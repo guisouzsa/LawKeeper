7d91193b5b050bfe8b264d028c643dc3ef5d84e1
guisouzsa <guilherme.souza103@aluno.ce.gov.br>
2025-11-13 23:20:26 -0300

    feat: criar migration de audiências
    Cria a tabela audiencias com os campos titulo, tipo, data_horario, local, descricao e timestamps.

---
+++ database/migrations/2025_11_14_014306_create_audiencias_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+   
+    public function up(): void
+    {
+        Schema::create('audiencias', function (Blueprint $table) {
+           $table->id();
+            $table->string('titulo');
+            $table->string('tipo');
+            $table->dateTime('data_horario');
+            $table->string('local');
+            $table->text('descricao')->nullable();
+            $table->timestamps();
+        });
+    }
+
+   
+    public function down(): void
+    {
+        Schema::dropIfExists('audiencias');
+    }
+};
